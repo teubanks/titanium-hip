2a74a80400464fbcbb146044411142757f69cc9f
Tracey Eubanks <tracey@bypassmobile.com>
2014-12-16 16:31:58 -0600

    Started client
    Also added Gemfile

--- lib/titanium_hip/cli.rb
+++ lib/titanium_hip/cli.rb
@@ -1,19 +1,28 @@
-api_key = 'adcW2us4r1dANOr3KPs5e6sxgL4N8MlDCqzx9TCy'
-message = {
-  notify: true,
-  message_format: 'text',
-  color: 'purple',
-  message: "@TFiddy sure thing"
-}
+module TitaniumHip
+  class CLI < ::Thor
+    include ::Thor::Actions
 
-headers = {
-  'Content-Type' =>'application/json',
-  'Authorization' => "Bearer #{api_key}"
-}
-uri = URI.parse('https://api.hipchat.com/v2/room/Team%20Doge/notification')
-https = Net::HTTP.new(uri.host)
-req = Net::HTTP::Post.new(uri.path, initheader = headers)
-req.body = message.to_json
-res = https.request(req)
-puts "Response #{res.code} #{res.message}: #{res.body}"
+    def message_room(room_name, message, options={})
+      puts "messaging room #{room_name} with message #{message}"
+    end
+  end
+end
+# api_key = 'adcW2us4r1dANOr3KPs5e6sxgL4N8MlDCqzx9TCy'
+# message = {
+#   notify: true,
+#   message_format: 'text',
+#   color: 'purple',
+#   message: "@TFiddy sure thing"
+# }
+# 
+# headers = {
+#   'Content-Type' =>'application/json',
+#   'Authorization' => "Bearer #{api_key}"
+# }
+# uri = URI.parse('https://api.hipchat.com/v2/room/Team%20Doge/notification')
+# https = Net::HTTP.new(uri.host)
+# req = Net::HTTP::Post.new(uri.path, initheader = headers)
+# req.body = message.to_json
+# res = https.request(req)
+# puts "Response #{res.code} #{res.message}: #{res.body}"
 
