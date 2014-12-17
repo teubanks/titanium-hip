7b68b3318e5d4781841b8235b04f4bf74420641d
Tracey Eubanks <tracey@bypassmobile.com>
2014-12-17 10:55:08 -0600

    Got a client installed
    The first iteration works

---
+++ lib/titanium_hip.rb
@@ -0,0 +1,2 @@
+module TitaniumHip
+end
--- lib/titanium_hip/cli.rb
+++ lib/titanium_hip/cli.rb
@@ -1,9 +1,31 @@
+require 'hipchat'
 module TitaniumHip
   class CLI < ::Thor
     include ::Thor::Actions
 
+    desc('message_room ROOMNAME MESSAGE', 'Send a message to a room')
     def message_room(room_name, message, options={})
-      puts "messaging room #{room_name} with message #{message}"
+      room_options = hipchat_options['rooms'].find {|key,val| key == room_name}
+      hipchat_client = client(room_options.last)
+      hipchat_client[room_name].send('ThatsNotMyName', message, notify: true)
+    end
+
+    desc 'server_mode', "Put Titanium Hip in server mode. Makes Titanium Hip listen for incoming connections. Use netcat or something similar to send commands"
+    option :port, type: :numeric, default: 7878, aliases: %w(-p), desc: 'Port to bind Titanium Hip server'
+    option :host, type: :string,  default: '127.0.0.1', aliases: %w(-h), desc: 'Host to bind Titanium Hip server'
+    def server_mode
+      "STARTING SERVER MODE!!!"
+    end
+
+    no_tasks do
+      def hipchat_options
+        yaml_file = File.expand_path('config/hipchat.yml')
+        YAML.load_file(yaml_file)
+      end
+
+      def client(room_api_key)
+        HipChat::Client.new(room_api_key, api_version: 'v2')
+      end
     end
   end
 end
